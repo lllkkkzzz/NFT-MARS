735c8a7b4fdd26795d6d9465264ea2cd9a71c648
lkx <Lin0112@mail.ustc.edu.cn>
2024-10-25 12:47:23 -0400

    wandb setting

--- Our_model/main.py
+++ Our_model/main.py
@@ -231,6 +231,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     NFT_MARS = Net(args)
-    wandb.init(project="YOUR_PROJECT", name=f'{args.collection}_{args.loss_alpha}', entity="YOUR_ENTITY", config={k: v for k, v in args._get_kwargs()})
+    wandb.init(project="nft", name=f'{args.collection}_{args.loss_alpha}', entity="k73623407-worcester-polytechnic-institute", config={k: v for k, v in args._get_kwargs()})
     NFT_MARS.run()
     wandb.finish()
